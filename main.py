b1ad16d055d02e9b984e15e589927cca9b6ed268
DeIdArOcHkA228 <drozhkoolesya@gmail.com>
2021-10-28 10:36:46 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import requests
+import os
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+import argparse
+
+
+def shorten_link(headers, link):
+  url_for_shorten_link = "https://api-ssl.bitly.com/v4/shorten"
+  payload = {"long_url": link}
+
+  response = requests.post(url_for_shorten_link, json=payload, headers=headers)
+  response.raise_for_status()
+  return response.json()["link"]
+
+
+def count_clicks(headers, link):
+  parsed_link = urlparse(link)
+  link_netloc_and_path = f"{parsed_link.netloc}{parsed_link.path}"
+  url_for_all_clicks = "https://api-ssl.bitly.com/v4/bitlinks/"\
+                       f"{link_netloc_and_path}/clicks/summary"
+  response = requests.get(url_for_all_clicks, headers=headers)
+  response.raise_for_status()
+  return response.json()["total_clicks"]
+
+
+def is_bitlink(headers, link):
+  parsed_link = urlparse(link)
+  link_netloc_and_path = f"{parsed_link.netloc}{parsed_link.path}"
+  url_for_verification = f"https://api-ssl.bitly.com/v4/bitlinks/{link_netloc_and_path}"
+  response = requests.get(url_for_verification, headers=headers)
+  return response.ok
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(
+    description='Описание что делает программа'
+    )
+  parser.add_argument('link', help=' Ваша ссылка')
+  args = parser.parse_args()
+  load_dotenv()
+  headers = {"Authorization": f"Bearer {os.getenv('BITLY_TOKEN')}"}
+  link = args.link
+  if is_bitlink(headers, link):
+    print(count_clicks(headers, link))
+  else:
+    print(shorten_link(headers, link))
